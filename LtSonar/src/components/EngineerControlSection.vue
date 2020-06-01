4579784e720eb537129d72f850d5aa1c128df230
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2020-05-31 18:14:30 -0700

    Component missing css animations and instruction images

---
+++ LtSonar/src/components/EngineerControlSection.vue
@@ -0,0 +1,85 @@
+<!--
+<some-tag-name title="My Component">
+    -- user can add their own HTML code here, replaces 'slot' component --
+</some-tag-name>
+
+@copyright (c) 2019. Scott Henshaw. All Rights Reserved.
+-->
+<template>
+
+    <section class="controls-section">  <!-- Just one main element per template -->
+        <h1>{{ title }}</h1>
+        <div class="control-line">
+            <div v-for="(data, index) in sectionData.topButtons" :key="index" class="control-button-special" v-bind:style="data"></div>
+            <!-- <div class="control-button-special" style="background: green; border-color: #FFFF7D;"></div>
+            <div class="control-button-special" style="background: yellow; border-color: #FFFF7D;"></div> -->
+        </div>
+        <hr>
+        <div class="control-line">
+            <div v-for="(data, index) in sectionData.bottomButtons" :key="index" class="control-button" v-bind:style="data"></div>
+            <!-- <div class="control-button" style="background: blue;"></div>
+            <div class="control-button" style="background: blue;"></div> -->
+        </div>
+
+    </section>
+
+</template>
+<script>
+    import Controller from '@/../lib/controller'
+
+    // import other components you use here...
+
+    class EngineerControlSectionController extends Controller {
+
+        constructor( name, subComponentList = []) {
+            super( name, subComponentList )
+            this.vm = {
+                someData: "Hello world"
+            }
+            this.props = { // props are passed in when using this component
+                title: String,
+                sectionData: Object
+            }
+        }
+
+        doIt( event ) {
+            // A method that does something to the props or viewModel, or global state
+        }
+    }
+
+    export default new EngineerControlSectionController('lsEngineerControlSection');
+
+</script>
+<style scoped>
+
+    .controls-section {
+        width: 23%;
+        height: 50vh;
+        padding: 20px;
+        background-color: cadetblue;
+    }
+
+    .control-line {
+        display: flex;
+        margin-top: 10%;
+        margin-bottom: 10%;
+        flex-direction: row;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+
+    .control-button {
+        border-radius: 50%;
+        width: 100px;
+        height: 100px;
+        border: 2px solid black;
+    }
+
+    .control-button-special {
+        border-radius: 50%;
+        width: 100px;
+        height: 100px;
+        border: 5px solid black;
+    }
+
+</style>
--- LtSonar/src/routes/Engineer.vue
+++ LtSonar/src/routes/Engineer.vue
@@ -5,14 +5,21 @@ Copyright (c) 2018. Scott Henshaw, Kibble Online Inc. All Rights Reserved.
 <template>
 
     <section class="engineer-container">
-        <div class="about">
+        <div class="control-panel">
             <h1 class="title">{{ name }}</h1>
+            <div class="controls-wrapper">
+                <ls-engineer-control-section v-bind:sectionData="firstSection" title="West"/>
+                <ls-engineer-control-section v-bind:sectionData="secondSection" title="North"/>
+                <ls-engineer-control-section v-bind:sectionData="thirdSection" title="South"/>
+                <ls-engineer-control-section v-bind:sectionData="fourthSection" title="East"/>
+            </div>
         </div>
     </section>
 
 </template>
 <script>
     import Controller from '@/../lib/controller'
+    import lsEngineerControlSection from '@/components/EngineerControlSection.vue';
 
     class EngineerController extends Controller {
 
@@ -20,30 +27,167 @@ Copyright (c) 2018. Scott Henshaw, Kibble Online Inc. All Rights Reserved.
             super( name, subComponentList );
             this.vm = {
                 name: 'Engineer control panel',
+                firstSection: {
+                    topButtons: [
+                        {
+                            background: 'red',
+                            "border-color": 'FFFF7D'
+                        },
+                        {
+                            background: 'yellow',
+                            "border-color": 'FFFF7D'
+                        },
+                        {
+                            background: 'green',
+                            "border-color": 'FFFF7D'
+                        },
+                    ],
+                    bottomButtons: [
+                        {
+                            background: 'green'
+                        },
+                        {
+                            background: 'blue'
+                        },
+                        {
+                            background: 'blue'
+                        }
+                    ]
+                },
+                secondSection: {
+                    topButtons: [
+                        {
+                            background: 'yellow',
+                            "border-color": 'FFD57D'
+                        },
+                        {
+                            background: 'red',
+                            "border-color": 'FFD57D'
+                        },
+                        {
+                            background: 'yellow',
+                            "border-color": 'FFD57D'
+                        },
+                    ],
+                    bottomButtons: [
+                        {
+                            background: 'green'
+                        },
+                        {
+                            background: 'red'
+                        },
+                        {
+                            background: 'blue'
+                        }
+                    ]
+                },
+                thirdSection: {
+                    topButtons: [
+                        {
+                            background: 'green',
+                            "border-color": 'B3B3B3'
+                        },
+                        {
+                            background: 'yellow',
+                            "border-color": 'B3B3B3'
+                        },
+                        {
+                            background: 'red',
+                            "border-color": 'B3B3B3'
+                        },
+                    ],
+                    bottomButtons: [
+                        {
+                            background: 'red'
+                        },
+                        {
+                            background: 'blue'
+                        },
+                        {
+                            background: 'yellow'
+                        }
+                    ]
+                },
+                fourthSection: {
+                    topButtons: [
+                        {
+                            background: 'green',
+                            "border-color": 'FFD57D'
+                        },
+                        {
+                            background: 'yellow',
+                            "border-color": 'B3B3B3'
+                        },
+                        {
+                            background: 'red',
+                            "border-color": 'FFFF7D'
+                        },
+                    ],
+                    bottomButtons: [
+                        {
+                            background: 'blue'
+                        },
+                        {
+                            background: 'green'
+                        },
+                        {
+                            background: 'blue'
+                        }
+                    ]
+                }
             }
         }
     }
 
-    export default new EngineerController('lsEngineer');
+    export default new EngineerController('lsEngineer', { lsEngineerControlSection });
 
 </script>
 <style scoped>
 /* Local styles for this template */
-    .about-container {
+    .control-container {
         display: inline-block;
         width: 100%;
-    }
-
-    .about {
-        margin:2vw;
-        border: 1px solid black;
-        background-color: lightgray;
-        color: black;
-        height: 78vh;
-        width: 80vw;
     }
 
     .title {
         text-shadow: 2px 2px #777;
     }
+
+    .controls-wrapper {
+        margin-top: 10px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        width: 80vw;
+    }
+
+    /*.controls-section {
+        width: 23%;
+        height: 50vh;
+        padding: 20px;
+        background-color: cadetblue;
+    }
+
+    .control-line {
+        display: flex;
+        margin-top: 10%;
+        margin-bottom: 10%;
+        flex-direction: row;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+
+    .control-button {
+        border-radius: 50%;
+        width: 100px;
+        height: 100px;
+        border: 2px solid black;
+    }
+
+    .control-button-special {
+        border-radius: 50%;
+        width: 100px;
+        height: 100px;
+        border: 5px solid black;
+    } */
 </style>
