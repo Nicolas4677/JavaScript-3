be83d962a2959e7518eb1ae825c4768873093997
Joel Drysdale <pg18joel@vfs.com>
2020-06-15 23:11:11 -0700

    Player Store Fix

--- LtSonar/src/model/Player.js
+++ LtSonar/src/model/Player.js
@@ -6,4 +6,15 @@ export default class Player {
     contructor(name) {        
         this.name = name;
     }
+
+    setTeam( team ) {
+        this.Red,
+        this.Blue
+    }
+    setRole( role ) {
+        this.Captain,
+        this.FirstOfficer,
+        this.Engineer,
+        this.Navigator
+    }
 }
